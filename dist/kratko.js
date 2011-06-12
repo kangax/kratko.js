7537edbf5023416479c958eea03bbc39f1dfd9b9
kangax <kangax@gmail.com>
2011-06-12 11:10:31 -0400

    Fix typo.

--- dist/kratko.js
+++ dist/kratko.js
@@ -108,8 +108,8 @@ TableViewer.prototype = {
 
     '.kratko-shim { position: fixed; top: 0; left: 0; width: 100%; height: 100%; background: rgba(0,0,0,0.4); z-index: 99 }' +
 
-    '.kratko-graph { position: fixed; top: 10px; right: 10px; z-index: 101;
-                    background: #fff; padding: 5px; box-shadow: 0px 0px 7px rgba(0,0,0,0.3); }' +
+    '.kratko-graph { position: fixed; top: 10px; right: 10px; z-index: 101;' +
+                    'background: #fff; padding: 5px; box-shadow: 0px 0px 7px rgba(0,0,0,0.3); }' +
 
     '.kratko-graph .block { display: inline-block; width: 12px; background: red; height: 100px; margin-right: 1px }' +
     '.kratko-graph .axis { border-top: 1px solid #ddd }' +
